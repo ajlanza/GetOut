157ae77d90efdffb033b325349cd5333db1732e7
ajlanza <61472953+ajlanza@users.noreply.github.com>
2020-04-26 18:00:31 -0400

    Get Out finalized
    App presents correctly on both mobile and desktop. Search works and event link work as well.

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 function watch() {
-    //See if the form is submitted
     $('form').submit(event =>{
         event.preventDefault();
-        let myLocation = $('#location').val();
+        const myLocation = $('#location').val();
         getEvents(myLocation);
     })
    }
-
+// Get events from tickemaster for the location the user has chosen.
 function getEvents(loc) {
     const baseURL = 'https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&apikey=9ILySSY2YO4e93LLEXVkLXAO93uY4YC2';
     const newURL = baseURL.concat(`&city=${loc}`);
@@ -20,13 +19,13 @@ function getEvents(loc) {
           }
     })
       .then(responseJson => displayResults(responseJson))
-      .catch(err => alert(`That event fetch didn't seem to work. ${err.message}`));
+      .catch(err => alert(`That location didn't seem to work. Please try again with a city name. ${err.message}`));
     }
 
+// Show the user the events returned by ticketmaster.    
 function displayResults(responseJson){
     // hits is the number of events returned from the search
-     let hits = responseJson._embedded.events.length;
-    // Display each of our events in a list item
+    const hits = responseJson._embedded.events.length;
     document.getElementById("events").innerHTML= `Select an event to see the venue.<br><br>`;
     for (let i = 0; i < hits; i++){
         let eventId = responseJson._embedded.events[i].id;
@@ -37,19 +36,20 @@ function displayResults(responseJson){
     }
 }
 
-// MAP JAVASCRIPT
+// Set up our map with mapbox API.
 mapboxgl.accessToken = 'pk.eyJ1IjoicmVkcnVtb3IiLCJhIjoiY2s5OTU2ZXZoMDh6bDNtbng4Mm54d3NuYSJ9.SKElWJx3uVl8sFBqvHskxA';
   let map = new mapboxgl.Map({
-    container: 'map', // Container ID
-    style: 'mapbox://styles/mapbox/streets-v11', // Map style to use
-    center: [-122.431297, 37.773972], // Starting position [lng, lat]
-    zoom: 12, // Starting zoom level
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11', 
+    center: [-73.756233, 42.652580], // Starting position [lng, lat]
+    zoom: 12, 
   });
-// MAP MARKER
-    let marker = new mapboxgl.Marker() // initialize a new marker
+// Put a marker on the map.
+    let marker = new mapboxgl.Marker()
     .setLngLat([-122.431297, 37.773972]) // Marker [lng, lat] coordinates
     .addTo(map); // Add the marker to the map
-  
+
+// Update the map with the event the user has clicked on.
 function updateMap(lat, lon) {
     document.getElementById('map').innerHTML="";
         map = new mapboxgl.Map({
@@ -58,11 +58,12 @@ function updateMap(lat, lon) {
         center: [lon, lat], // Starting position [lng, lat]
         zoom: 14, // Starting zoom level
   });
-  marker = new mapboxgl.Marker() // initialize a new marker
-    .setLngLat([lon, lat]) // Marker [lng, lat] coordinates
-    .addTo(map); // Add the marker to the map
+  marker = new mapboxgl.Marker() 
+    .setLngLat([lon, lat]) 
+    .addTo(map);
 }
 
+// Submit a request to ticketmaster API for the event the user has chosen.
 function getDetails(name, id) {
     const baseURL = 'https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&apikey=9ILySSY2YO4e93LLEXVkLXAO93uY4YC2';
     const newURL = baseURL.concat(`&id=${id}`);
@@ -76,9 +77,10 @@ function getDetails(name, id) {
           }
     })
       .then(responseJson => displayDetails(responseJson))
-      .catch(err => alert(`That id fetch didn't seem to work. ${err.message}`));
+      .catch(err => alert(`That event didn't seem to work. Please choose a different one. ${err.message}`));
     }
 
+// Show the user the venue the event is at and change the background image to one supplied by ticketmaster.
 function displayDetails(responseJson) {
     let venue = responseJson._embedded.events[0]._embedded.venues[0].name;
     let name = responseJson._embedded.events[0].name;
@@ -88,4 +90,5 @@ function displayDetails(responseJson) {
     container.style.backgroundImage = `url('${bandImage}')`;
     container.style.backgroundSize = `cover`;
 }
+
 $(watch);
